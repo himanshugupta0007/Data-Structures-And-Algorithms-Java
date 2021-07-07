444444b681a54385401423b942c8b5b2c1555f87
Himanshu <himanshugupta0007@gmail.com>
2021-07-06 19:36:56 -0500

    Updated example

--- src/com/ds/algo/sorting/BubbleSort.java
+++ src/com/ds/algo/sorting/BubbleSort.java
@@ -1,46 +1,52 @@
-/**
- * 
- */
 package com.ds.algo.sorting;
 
 /**
- * @author himanshugupta
- *
+ * This is an example class for Bubble Sort
+ * 
+ * @author Himanshu Gupta
+ * @since 6 July, 20201
+ * @version 1.0
+ * 
  */
 public class BubbleSort {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		
-		int array[] = {1,55,123,77,45,-44,-98,234, 0, 544, -998, 123};
+
+		// define your array
+		int array[] = { 1, 55, 123, 77, 45, -44, -98, 234, 0, 544, -998, 123 };
+
+		// define counter to identify number of iterations for sorting
 		int count = 0;
-			
+
+		System.out.println("Original Array :-");
+		//print original array
 		for (int i : array) {
 			System.out.println(i);
 		}
-		System.out.println("**********************");
-		
-		for(int i = 0; i < array.length;i++) {
-			for(int y = i+1; y < array.length; y++)
-			{
-				if(array[i] < array[y]) {
+
+		for (int i = 0; i < array.length; i++) {
+			for (int y = i + 1; y < array.length; y++) {
+				
+				//swap the elements after comparison
+				if (array[i] > array[y]) {
 					int temp = array[i];
 					array[i] = array[y];
 					array[y] = temp;
 				}
-				
-				if(y == i) {
+
+				if (y == i) {
 					continue;
 				}
 				count++;
 			}
 		}
-	
+		
+		System.out.println("Sorted Array :-");
+		//print sorted array
 		for (int i : array) {
 			System.out.println(i);
 		}
+		
 		System.out.println("Number Of Iterations:" + count);
 	}
 
